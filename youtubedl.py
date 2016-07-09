059c01307bbe2e73f4425b0bc7fb9cfd1f486f48
JiasHuang <jias.tw@gmail.com>
2016-07-09 20:04:34 +0800

    modified:   youtubedl.py

--- youtubedl.py
+++ youtubedl.py
@@ -52,7 +52,9 @@ def extractURL_keepvid(url):
     return None
 
 def extractURL(url):
-    return extractURL_keepvid(url)
+    if re.search(r'youtube', url):
+        return extractURL_keepvid(url)
+    return extractURL_def(url)
 
 def extractSUB_def(url):
 
@@ -84,4 +86,6 @@ def extractSUB_keepvid(url):
     return None
 
 def extractSUB(url):
-    return extractSUB_keepvid(url)
+    if re.search(r'youtube.com', url):
+        return extractSUB_keepvid(url)
+    return extractSUB_def(url)
