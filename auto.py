46ff6124efbaee8706fb30f5e60f80734e4afb62
JiasHuang <jias.tw@gmail.com>
2017-01-04 22:31:11 +0800

    modified:   auto.py

--- auto.py
+++ auto.py
@@ -3,13 +3,22 @@
 
 import os
 import re
+import time
 
 import goodtv
 import xdef
 
+def countdown():
+    print('tasks done, now sleeping for 10 seconds')
+    for i in xrange(10, 0, -1):
+        time.sleep(1)
+        print(i)
+    return
+
 def main():
     os.chdir(xdef.workdir)
     goodtv.download()
+    countdown()
     return
 
 if __name__ == '__main__':
