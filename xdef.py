efc256e8e4657caa1c5612aa5afaa95b41fe1161
JiasHuang <jias.tw@gmail.com>
2017-01-15 12:22:04 +0800

    201701 pi

--- xdef.py
+++ xdef.py
@@ -21,4 +21,4 @@ smp      = 'smplayer -fullscreen'
 mpv      = 'mpv --fs --ontop --cache=16384 --cache-secs=10'
 mpv_ytdl = '--ytdl=no --cookies --cookies-file=%s' %(cookies)
 mpv_rpi  = 'mpv --vo=rpi --fs --ontop --cookies --cookies-file=%s' %(cookies)
-omxp     = 'omxplayer -b -o hdmi -I'
+omxp     = 'omxplayer -b -o both -I'
