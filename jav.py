b2444b3a3c916e749aabfd7ae40f4a6aa7fa0ef5
jias_huang <jias_huang@realtek.com>
2017-06-19 18:40:59 +0800

    add cryptoJSAESJson.js
    :Release Notes:
    
    :Detailed Notes:
    - Problem :
    - RootCause :
    - Solution :
    - Author :
    - Build :
    
    :Testing Preformed:
    How to Test :

--- jav.py
+++ jav.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import re
 import base64
 import hashlib
@@ -19,10 +20,27 @@ def checkVideoURL(url):
 def load(url, local=None, options=None):
     return xurl.load2(url, local, options)
 
+def decryptJSCode(url):
+    txt = load(url)
+    if re.search(r'<script src="https://jqd.cdn-qdnetwork.com', txt):
+        m = re.search(r'_fallback = \'(.*?)\'', txt)
+        if m:
+            _fallback = m.group(1)
+            token = 'avcms.co'
+            dirname = os.path.dirname(os.path.realpath(__file__))
+            jscode = xurl.readLocal(dirname+'/cryptoJSAESJson.js') + '\n'
+            jscode += 'console.log(CryptoJSAESdecrypt(\'%s\', \'%s\'));' %(_fallback, token)
+            output = jsunpack.executeJSCode(jscode)
+            return output
+    return None
+
 def getSource(url):
     links = xurl.getIFrame(url)
     for link in links:
         if checkVideoURL(link):
             return link
+    link = decryptJSCode(url)
+    if link:
+        return link
     return None
 
