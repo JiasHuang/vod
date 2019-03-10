8c55a2c6663224cdef50256f61286029dc3eddb3
jiashuang <jias.tw@gmail.com>
2019-03-10 22:46:10 +0800

    refine getRedirectLink for extra cookies

--- xsrc.py
+++ xsrc.py
@@ -46,9 +46,15 @@ def parseParameters(url, key, ref):
 def getRedirectLink(url):
     cmd = 'curl -I %s' %(url)
     output = subprocess.check_output(cmd, shell=True)
+    cookies = []
+    cookies_str = None
+    for m in re.finditer('Set-Cookie: (.*?)(\n|\r)', output):
+        cookies.append(m.group(1))
+    if len(cookies) > 0:
+        cookies_str = '; '.join(cookies)
     m = re.search('Location: (.*?)\n', output)
     if m:
-        return m.group(1)
+        return m.group(1), cookies_str
     return url
 
 def removeHashTag(url):
@@ -117,8 +123,10 @@ def getSource(url, key=None, ref=None):
             src, cookies = youtubedl.extractURL(url, key=key, ref=ref)
             srcRef = url
             if re.search('dailymotion', src):
-                src = getRedirectLink(src)
+                src, extra_cookies = getRedirectLink(src)
                 src = removeHashTag(src)
+                if extra_cookies:
+                    cookies = extra_cookies
 
     if not src:
         src = getIframeSrc(url)
