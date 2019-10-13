482dec8694ce93398a6af5f45e888563043e4ce9
jiashuang <jias.tw@gmail.com>
2019-10-13 13:30:12 +0800

    fix pangzitv

--- web/extractors/pangzitv.py
+++ web/extractors/pangzitv.py
@@ -10,7 +10,7 @@ VALID_URL = r'pangzitv'
 def extract(url):
     objs = []
     if re.search(r'vod-detail-id', url):
-        for m in re.finditer(r'href="(/\?m=vod-play-id-[^"]*)" title="(.*?)"', load(url)):
+        for m in re.finditer(r'href="(/.*?vod-play-id-[^"]*)" title="(.*?)"', load(url)):
             ep_url, ep_title = 'http://www.pangzitv.com' + m.group(1), m.group(2)
             objs.append(entryObj(ep_url, ep_title))
     elif re.search(r'(vod-type-id|vod-list-id)', url):
