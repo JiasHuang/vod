caf9b0144ffd9ea2f5b00eba58514baa710daa8d
jiashuang <jias.tw@gmail.com>
2020-02-01 16:35:15 +0800

    update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -9,7 +9,13 @@ import xurl
 def getSource(url):
     txt = xurl.curl(url)
     result = []
-    for m in re.finditer(r'"url":"([^"]*)"', txt):
-        result.append(m.group(1))
+    m = re.search(r'av(\d+)', url)
+    avid = m.group(1) if m else None
+    m = re.search(r'"cid":(\d+)', txt)
+    cid = m.group(1) if m else None
+    if avid and cid:
+        api_url = 'https://api.bilibili.com/x/player/playurl?cid=%s&avid=%s&otype=json' %(cid, avid)
+        for m in re.finditer(r'"url":"([^"]*)"', xurl.curl(api_url)):
+            result.append(m.group(1).decode('unicode_escape'))
     return youtubedl.genM3U(url, result)
 
