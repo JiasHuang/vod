2cecfa03fd30da7c530a2072fd0bcf1674594620
jiashuang <jias.tw@gmail.com>
2020-02-09 17:06:06 +0800

    refine extractors/bilibili.py

--- extractors/bilibili.py
+++ extractors/bilibili.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import json
 
 import xurl
 
@@ -21,8 +22,10 @@ def getSource(url):
     cid = m.group(1) if m else None
     if avid and cid:
         api_url = 'https://api.bilibili.com/x/player/playurl?avid=%s&cid=%s&otype=json' %(avid, cid)
-        for m in re.finditer(r'"url":"([^"]*)"', xurl.curl(api_url)):
-            result.append(m.group(1).decode('unicode_escape'))
+        api_txt = xurl.curl(api_url)
+        data = json.loads(api_txt)
+        for d in data['data']['durl']:
+            result.append(d['url'])
     local = xurl.genLocal(url, prefix='vod_list_', suffix='.m3u8')
     xurl.saveM3U8(local, result)
     return local
