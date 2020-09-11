6bbd2b430b408d43f0874e4e133a44fece71f438
jiashuang <jias.tw@gmail.com>
2020-09-11 22:27:13 +0800

    fix painku

--- extractors/pianku.py
+++ extractors/pianku.py
@@ -9,6 +9,6 @@ VALID_URL = r'pianku\.tv'
 
 def getSource(url, fmt, ref):
     txt = xurl.curl(url)
-    m = re.search(r'url: \'(.*?)\'', txt)
+    m = re.search(r'geturl\(\'(.*?)\'\)', txt)
     return m.group(1) if m else None
 
