ce949c8b61c711c3dfae7edc7f6208aa2790691e
jiashuang <jias.tw@gmail.com>
2020-11-17 10:21:50 +0800

    fix sync.sh

--- sync.sh
+++ sync.sh
@@ -2,6 +2,11 @@
 
 cd /opt/
 
-sudo wget https://youtube-dl.org/downloads/latest/youtube-dl-2020.11.12.tar.gz
-sudo tar zxf youtube-dl-2020.11.12.tar.gz
-sudo ln -sfn /opt/youtube-dl/youtube_dl/__main__.py /usr/local/bin/youtube-dl
+if [ -d "youtube-dl" ]
+then
+	cd youtube-dl
+	sudo git pull
+else
+	sudo git clone https://github.com/ytdl-org/youtube-dl.git
+	sudo ln -sfn /opt/youtube-dl/youtube_dl/__main__.py /usr/local/bin/youtube-dl
+fi
