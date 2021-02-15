830f207be5ab8480fb7e3d73aab355674b50052d
jiashuang <jias.tw@gmail.com>
2021-02-15 18:06:48 +0800

    add gimy.py

---
+++ extractors/gimy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+import xurl
+
+VALID_URL = r'gimy\.co'
+
+def getSource(url, fmt, ref):
+    m = re.search(r'"url":"(http[^"]*)"', xurl.load(url))
+    if m:
+        return m.group(1).replace('\\', '')
+    return None
---
+++ web/extractors/gimy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re
+
+from .utils import *
+
+VALID_URL = r'gimy\.co'
+
+def extract(url):
+    objs = []
+    if re.search(r'/cat/', url):
+        for m in re.finditer(r'data-original="([^"]*)" href="([^"]*)" title="([^"]*)"', load(url)):
+            image = m.group(1)
+            link = urljoin(url, m.group(2))
+            title = m.group(3)
+            objs.append(pageObj(link, title, image))
+    elif re.search(r'/vod/', url):
+        m = re.search(r'<div class="playlist">(.*?)</div>', load(url), re.DOTALL|re.MULTILINE)
+        if m:
+            playlist = m.group()
+            for x in re.finditer(r'<li><a href="([^"]*)">(.*?)</a></li>', playlist):
+                link = urljoin(url, x.group(1))
+                title = x.group(2)
+                objs.append(videoObj(link, title))
+    return objs
