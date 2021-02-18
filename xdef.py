1606342244fc9138dac922e5139eabda49c36f6a
jiashuang <jias.tw@gmail.com>
2021-02-18 09:30:12 +0800

    refine dl

--- xdef.py
+++ xdef.py
@@ -9,7 +9,7 @@ dldir    = '/var/tmp/'
 fifo     = codedir+'vod.fifo'
 log      = workdir+'vod_%s.log' %(os.getuid())
 mpv      = 'mpv --fs --ontop --ytdl=no --demuxer-lavf-o=protocol_whitelist=\\"file,http,https,tcp,tls,crypto\\" --input-file=%s --save-position-on-quit' %(fifo)
-omxp     = 'omxplayer -b -o both -I'
+omxp     = 'omxplayer -b -o both -I -s'
 ffplay   = 'ffplay -fs -window_title ffplay'
 playlist = workdir+'vod_%s_playlist' %(os.getuid())
 playbackMode = workdir+'vod_%s_playbackMode' %(os.getuid())
